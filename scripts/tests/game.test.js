16d3e6df0d5ef5944883641aadf2e63b7375085c
JoseMGuerra <Josem_guerra@hotmail.com>
2022-09-17 07:44:42 +0000

    Add newGame function and test it

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -1,7 +1,7 @@
 /**
  * @jest-environment jsdom
  */
-const { game } = require("../game");
+const { game, newGame, showScore, addTurn } = require("../game");
 
 beforeAll(() => {
   let fs = require("fs");
@@ -26,5 +26,27 @@ describe("game object contains the correct keys", () => {
   });
   test("choices to contain the correct ids", () => {
     expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
+  });
+});
+
+describe("newGame function works correctly", () => {
+  beforeAll(() => {
+    game.score = 42;
+    game.currentGame = ["button1", "button2"];
+    game.playerMoves = ["button1", "button2"];
+    document.getElementById("score").innerText = "42";
+    newGame();
+  });
+  test("should set game score to zero", () => {
+    expect(game.score).toEqual(0);
+  });
+  test("should be one move in the computer's game array", () => {
+    expect(game.currentGame.length).toEqual(1);
+  });
+  test("should clear game playerMoves array", () => {
+    expect(game.playerMoves.length).toEqual(0);
+  });
+  test("should display 0 for the element with id of score", () => {
+    expect(document.getElementById("score").innerText).toEqual(0);
   });
 });
