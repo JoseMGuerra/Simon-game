16b3433338922f702cc8d9d0cbbda8bcb888c113
JoseMGuerra <Josem_guerra@hotmail.com>
2022-09-17 05:20:50 +0000

    Add game object and test for keys

---
+++ scripts/game.js
@@ -0,0 +1,16 @@
+// Structure:
+// game = {
+// score: - integer
+// currentGame: - array
+// playerMoves: - array
+// choices: - array
+//}
+
+let game = {
+  score: 0,
+  currentGame: [],
+  playerMoves: [],
+  choices: ["button1", "button2", "button3", "button4"],
+};
+
+module.exports = { game };
