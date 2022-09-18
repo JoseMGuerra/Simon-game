a37b77016fb0678ac0ecc705d1945348284c0c79
JoseMGuerra <Josem_guerra@hotmail.com>
2022-09-18 08:09:06 +0000

    Add showTurns function and test it

--- scripts/game.js
+++ scripts/game.js
@@ -2,6 +2,7 @@ let game = {
   score: 0,
   currentGame: [],
   playerMoves: [],
+  turnNumber: 0,
   choices: ["button1", "button2", "button3", "button4"],
 };
 
@@ -15,7 +16,7 @@ function newGame() {
 function addTurn() {
   game.playerMoves = [];
   game.currentGame.push(game.choices[Math.floor(Math.random() * 4)]);
-  // showTurns();
+  showTurns();
 }
 
 function showScore() {
@@ -25,7 +26,25 @@ function lightsOn(circ) {
   document.getElementById(circ).classList.add("light");
   setTimeout(() => {
     document.getElementById(circ).classList.remove("light");
-  }, 00);
+  }, 400);
 }
 
-module.exports = { game, newGame, showScore, addTurn, lightsOn };
+function showTurns() {
+  game.turnNumber = 0;
+  let turns = setInterval(() => {
+    lightsOn(game.currentGame[game.turnNumber]);
+    game.turnNumber++;
+    if (game.turnNumber >= game.currentGame.length) {
+      clearInterval(turns);
+    }
+  }, 800);
+}
+
+module.exports = {
+  game,
+  newGame,
+  showScore,
+  addTurn,
+  lightsOn,
+  showTurns,
+};
